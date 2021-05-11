e0758af0a7fd0dd991bb22394a7657ce0df41a1e
itzmesk <83900082+itzmesk@users.noreply.github.com>
2021-05-11 16:44:35 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,60 @@
+class script(object):
+
+
+    START_MSG = """ <b>Hi {}
+
+You can call this as Movies Master Auto Filter Bot 
+
+I 𝐖𝐢𝐥𝐥 𝐒𝐞𝐚𝐫𝐜𝐡 𝐌𝐨𝐯𝐢𝐞𝐬 𝐔𝐬𝐢𝐧𝐠 𝐏𝐡𝐫𝐚𝐬𝐞 𝐖𝐨𝐫𝐝 𝐚𝐧𝐝 𝐆𝐢𝐯𝐞 𝐌𝐨𝐯𝐢𝐞𝐬 𝐚𝐬 𝐁𝐮𝐭𝐭𝐨𝐧 𝐋𝐢𝐧𝐤.
+
+ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ👨🏻‍💻: @itz_mesk
+
+ᴘᴏᴡᴇʀᴇᴅ ʙʏ: 🌀 @MoviesMasterLinks 🌀
+
+For more click <i>help</i></b>"""
+
+
+    HELP_MSG = """<b>How to use the bot??</b>
+
+<i>
+* Add bot to your group with admin rights.
+ 
+* Add bot to channels which you want to link with <b>all admin rights</b>!
+</i>
+
+
+<b>Bot Commands - Works in Group only</b>
+(You need to be a Auth User in order to use these commands)
+
+* <code>/add channelid</code>  -  Links channel to your group.
+or
+* <code>/add @channelusername</code> - Links channel to your group.
+
+<i>NOTE : You can get your channel ID from @ChannelidHEXbot </i>
+
+
+* <code>/del channelid</code>  -  Delinks channel from group
+or
+* <code>/del @channelusername</code>  -  Delinks channel from group
+
+<i>NOTE : You can get connected channel details by <code>/filterstats</code> </i>
+
+
+* <code>/delall</code>  -  Removes all connected channels and filters from group!
+
+<i>Note : Dont add command delete bots in group! Otherwise, delall command wont work</i>
+
+
+* <code>/filterstats</code>  -  Check connected channels and number of filters.
+
+
+
+No need add each filter again!
+Bot will automatically search for your files and give links to that!
+
+
+    ABOUT_MSG = """⭕️<b>My Name : Movies Master Filter Bot V2</b>
+    
+⭕️<b>Creater :</b> @itz_mesk
+
+"""
